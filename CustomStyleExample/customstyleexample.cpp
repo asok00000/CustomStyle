df0ec1db6bc837e09e2cfd8967fddc359fe37e67
humm <asok00000@qq.com>
2018-04-12 19:29:56 +0800

    1.add bootstrap button and combobox

--- CustomStyleExample/customstyleexample.cpp
+++ CustomStyleExample/customstyleexample.cpp
@@ -1,7 +1,13 @@
 #include "customstyleexample.h"
-
+#include <QComboBox>
+#include <QStyledItemDelegate>
 CustomStyleExample::CustomStyleExample(QWidget *parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
+	QList<QComboBox *> allComboBox = findChildren<QComboBox*>();
+	Q_FOREACH(QComboBox*_c, allComboBox)
+	{
+		_c->setItemDelegate(new QStyledItemDelegate());
+	}
 }
